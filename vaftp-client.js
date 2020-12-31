56baecf15cdc7f6881ee70bbb727dd11cd8d11c5
Richard Audette <richard_audette@yahoo.ca>
2020-12-31 12:53:04 -0500

    Logic working, testing with device remains

--- vaftp-client.js
+++ vaftp-client.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const { exit } = require('process');
+const { exit, send } = require('process');
+var player = require('play-sound')(opts = {})
 
 if (typeof process.argv[2] !== 'undefined') {
     file = process.argv[2]
@@ -11,7 +12,6 @@ else {
 }
 
 let filebuffer=[];
-let filebuffername=[];
 
 fs.readFile(file, (err, data) => {
     if (err) {
@@ -41,43 +41,105 @@ fs.readFile(file, (err, data) => {
 
 function filebuilder(asciival) {
     console.log(asciival)
-    if ((asciival>=1000) && (asciival<2000)) {
-        filebuffername.push(asciival-1000)
+    filebuffer.push(asciival)
+    if (asciival==2000) {
+        launchuploadfileskill()
     }
-    else if (asciival==2000) {
-        writefile()
+}
+
+var readytoplaysound = true;
+var launched=false
+var currentbyte=0;
+var currentdigit=0;
+
+function launchuploadfileskill() {
+    var soundfiletoplay='';
+    //console.log("ready:"+readytoplaysound+" current byte:"+currentbyte+" currentdigit:"+currentdigit)
+    if (currentbyte<filebuffer.length) {
+        if (readytoplaysound==true) {
+            console.log("launched:"+launched+" ready:"+readytoplaysound+" current byte:"+currentbyte+" currentdigit:"+currentdigit)
+            readytoplaysound=false
+            if (launched==false) {
+                launched=true;
+                soundfiletoplay='./media/launch.mp3';
+            }
+            else {
+                if (currentdigit==0) {
+                    //seems like a waste, but the easiest way to pause 
+                    soundfiletoplay='./media/pause.mp3'
+                    currentdigit++;        
+                }
+                else if (currentdigit==0) {
+                    soundfiletoplay='./media/addbyte.mp3'
+                    currentdigit++;        
+                }
+                else if (currentdigit==1) {
+                    if (filebuffer[currentbyte]>=1000) {
+                        soundfiletoplay=getfilename(Math.floor(filebuffer[currentbyte]/1000)%10)
+                    }
+                    else {
+                        readytoplaysound=true; //as there is no digit to be played here
+                    }
+                    currentdigit++;        
+                }
+                else if (currentdigit==2) {
+                    if (filebuffer[currentbyte]>=100) {
+                        soundfiletoplay=getfilename(Math.floor(filebuffer[currentbyte]/100)%10)        
+                    }
+                    else {
+                        readytoplaysound=true; //as there is no digit to be played here
+                    }
+                    currentdigit++;        
+                }
+                else if (currentdigit==3) {
+                    if (filebuffer[currentbyte]>=10) {
+                        soundfiletoplay=getfilename(Math.floor(filebuffer[currentbyte]/10)%10)        
+                    }
+                    else {
+                        readytoplaysound=true; //as there is no digit to be played here
+                    }
+                    currentdigit++;        
+                }
+                else if (currentdigit==4) {
+                    if (filebuffer[currentbyte]>=0) {
+                        soundfiletoplay=getfilename(filebuffer[currentbyte]%10)        
+                        currentbyte++;        
+                    }
+                    currentdigit=0;
+                }
+            }
+            if (soundfiletoplay.length>1) {
+                console.log("Playing "+soundfiletoplay)
+                player.play(soundfiletoplay, function(err){
+                    if (err) throw err
+                    readytoplaysound = true;
+                })
+            }
+        }
+    }
+    if ((readytoplaysound==true)&&(currentbyte==filebuffer.length)) {
+        console.log("Done!")
+        exit(0)
     }
     else {
-        filebuffer.push(asciival)
+        setTimeout(launchuploadfileskill, 100)    
     }
 }
 
-function writefile() {
-    var filename="";
-    for (var i = 0; i < filebuffername.length; i++) {
-        filename = filename+String.fromCharCode(filebuffername[i])
-    }
-    var data = new Uint8Array(filebuffer.length)
-    for (var i = 0; i < filebuffer.length; i++) {
-        data[i]=filebuffer[i]
-    }
-    filepath="./files/"+filename
-
-    if (fs.existsSync(filepath)) {
-        var dupefileindex=0;
-        while (fs.existsSync(filepath+dupefileindex)) {
-            dupefileindex++;
-        }
-        filepath=filepath+dupefileindex
-   
-    }
-
-    fs.writeFile(filepath,data,Uint8Array,  function (err) {
-        if (err) return console.log(err.message);
-        console.log('Done');
-      });
-}
-
-function done() {
-    exit(0);
+function getfilename(number) {
+    switch (number) {
+        case 0: return './media/zero.mp3'
+        case 1: return './media/one.mp3'
+        case 2: return './media/two.mp3'
+        case 3: return './media/three.mp3'
+        case 4: return './media/four.mp3'
+        case 5: return './media/five.mp3'
+        case 6: return './media/six.mp3'
+        case 7: return './media/seven.mp3'
+        case 8: return './media/eight.mp3'
+        case 9: return './media/nine.mp3'
+        default:
+          console.log('Error - no match');
+          exit(1)  
+      }
 }
